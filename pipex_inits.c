dafc6f2cc2fea8f761020a8f65e17bc5dcf2c1b7
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-12-21 14:33:31 -0300

    function that doest the validation of each command by looking for it in each directory specified in PATH environment variable

--- pipex_inits.c
+++ pipex_inits.c
@@ -6,12 +6,15 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/09 16:59:42 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/12/20 18:41:51 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/12/21 14:16:14 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "./includes/pipex.h"
 
+static char	*get_path_env(char **envp);
 static void	set_cmds(t_pipex *pipex, char **argv);
+static void	set_program_path(t_pipex *pipex, char **envp);
+static void	where_is(char **splitted_path, t_pipex *pipex);
 
 void	init_pipex(t_pipex *pipex, int argc, char **argv, char **envp)
 {
@@ -26,9 +29,68 @@ void	init_pipex(t_pipex *pipex, int argc, char **argv, char **envp)
 		exit(1);
 	}
 	pipex->commands = ft_calloc(sizeof(char **), pipex->n_cmds + 1);
-	pipex->cmd_name = ft_calloc(sizeof(char *), pipex->n_cmds + 1);
-	pipex->abs_path = ft_calloc(sizeof(char *), pipex->n_cmds + 1);
+	pipex->program_path = ft_calloc(sizeof(char *), pipex->n_cmds + 1);
 	set_cmds(pipex, argv);
+	set_program_path(pipex, envp);
+}
+
+static void	set_program_path(t_pipex *pipex, char **envp)
+{
+	char	*path_env_var;
+	char	**splitted_path_env;
+
+	path_env_var = get_path_env(envp);
+	splitted_path_env = ft_split(path_env_var, ':');
+	free(path_env_var);
+	format_splitted_path_env(splitted_path_env);
+	where_is(splitted_path_env, pipex);
+	ft_free_matrix((void **)splitted_path_env);
+}
+
+static void	where_is(char **splitted_path, t_pipex *pipex)
+{
+	int		i;
+	int		j;
+	char	*cmd_to_eval;
+
+	i = 0;
+	while (i < pipex->n_cmds)
+	{
+		j = 0;
+		while (splitted_path[j] != NULL)
+		{
+			cmd_to_eval = ft_strjoin(splitted_path[j], pipex->commands[i][0]);
+			if (access(cmd_to_eval, F_OK | X_OK) == 0)
+			{
+				pipex->program_path[i] = ft_strdup(cmd_to_eval);
+				free(cmd_to_eval);
+				break ;
+			}
+			else
+				pipex->program_path[i] = NULL;
+			free(cmd_to_eval);
+			j++;
+		}
+		i++;
+	}
+}
+
+static char	*get_path_env(char **envp)
+{
+	int		i;
+	char	*path_line;
+
+	i = 0;
+	while (envp[i] != NULL)
+	{
+		if (ft_strncmp(envp[i], "PATH=", 5) == 0)
+		{
+			path_line = ft_strdup(envp[i]);
+			return (path_line);
+		}
+		i++;
+	}
+	return (NULL);
 }
 
 static void	set_cmds(t_pipex *pipex, char **argv)
