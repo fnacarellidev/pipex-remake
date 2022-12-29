dc9e42d5dcf00e0dc98eacecbef7e72701794957
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-12-29 11:14:23 -0300

    fix: program no longer executes first command if infile doesn't exists or doesn't have permissions to read it

--- pipex_mandatory.c
+++ pipex_mandatory.c
@@ -6,16 +6,15 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 15:25:17 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/12/28 18:10:41 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/12/29 11:12:15 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "includes/pipex.h"
-#include "libft/libft.h"
 
+static void	close_fds(t_pipex *pipex);
 static int	quit_program(t_pipex *pipex);
 static void	run_first_cmd(t_pipex *pipex, char **envp);
 static void	run_second_cmd(t_pipex *pipex, char **envp);
-static void	print_err(char *str);
 
 int	main(int argc, char **argv, char **envp)
 {
@@ -25,13 +24,17 @@ int	main(int argc, char **argv, char **envp)
 	validate_files(argc, argv, envp);
 	init_pipex(&pipex, argc, argv, envp);
 	pipe(pipex.kernel_fd);
-	run_first_cmd(&pipex, envp);
+	if (pipex.infile_fd != -1)
+	{
+		run_first_cmd(&pipex, envp);
+		close(pipex.infile_fd);
+	}
 	close(pipex.kernel_fd[1]);
-	close(pipex.infile_fd);
 	run_second_cmd(&pipex, envp);
 	close(pipex.kernel_fd[0]);
 	close(pipex.outfile_fd);
-	wait(NULL);
+	if (pipex.infile_fd != -1)
+		wait(NULL);
 	wait(NULL);
 	return_code = quit_program(&pipex);
 	return (return_code);
@@ -44,26 +47,16 @@ static void	run_first_cmd(t_pipex *pipex, char **envp)
 	{
 		if (pipex->program_path[0] == NULL)
 		{
-			print_err(pipex->commands[0][0]);
+			close_fds(pipex);
 			ft_free_matrix_size_n((void **)pipex->program_path, pipex->n_cmds);
 			ft_free_spatial_matrix((void ***)pipex->commands);
 			exit(127);
 		}
 		dup2(pipex->infile_fd, 0);
 		dup2(pipex->kernel_fd[1], 1);
-		close(pipex->infile_fd);
-		close(pipex->outfile_fd);
-		close(pipex->kernel_fd[0]);
-		close(pipex->kernel_fd[1]);
+		close_fds(pipex);
 		execve(pipex->program_path[0], pipex->commands[0], envp);
 	}
-}
-
-static void	print_err(char *str)
-{
-	if (str != 0)
-		ft_printf("%s: command not found\n", str);
-	return ;
 }
 
 static void	run_second_cmd(t_pipex *pipex, char **envp)
@@ -73,29 +66,29 @@ static void	run_second_cmd(t_pipex *pipex, char **envp)
 	{
 		if (pipex->program_path[1] == NULL)
 		{
-			print_err(pipex->commands[1][0]);
+			close_fds(pipex);
 			ft_free_matrix_size_n((void **)pipex->program_path, pipex->n_cmds);
 			ft_free_spatial_matrix((void ***)pipex->commands);
 			exit(127);
 		}
 		dup2(pipex->kernel_fd[0], 0);
 		dup2(pipex->outfile_fd, 1);
-		close(pipex->infile_fd);
-		close(pipex->outfile_fd);
-		close(pipex->kernel_fd[0]);
-		close(pipex->kernel_fd[1]);
+		close_fds(pipex);
 		execve(pipex->program_path[1], pipex->commands[1], envp);
 	}
 }
 
 static int	quit_program(t_pipex *pipex)
 {
-	/* close(pipex->infile_fd); */
-	/* close(pipex->outfile_fd); */
-	/* close(pipex->kernel_fd[0]); */
-	/* close(pipex->kernel_fd[1]); */
+	if (pipex->program_path[0] == NULL)
+	{
+		if (pipex->commands[0][0] != 0)
+			ft_printf("%s: command not found\n", pipex->commands[0][0]);
+	}
 	if (pipex->program_path[1] == NULL)
 	{
+		if (pipex->commands[1][0] != 0)
+			ft_printf("%s: command not found\n", pipex->commands[1][0]);
 		ft_free_matrix_size_n((void **)pipex->program_path, pipex->n_cmds);
 		ft_free_spatial_matrix((void ***)pipex->commands);
 		return (127);
@@ -103,4 +96,13 @@ static int	quit_program(t_pipex *pipex)
 	ft_free_matrix_size_n((void **)pipex->program_path, pipex->n_cmds);
 	ft_free_spatial_matrix((void ***)pipex->commands);
 	return (0);
+}
+
+static void	close_fds(t_pipex *pipex)
+{
+	if (pipex->infile_fd != -1)
+		close(pipex->infile_fd);
+	close(pipex->outfile_fd);
+	close(pipex->kernel_fd[0]);
+	close(pipex->kernel_fd[1]);
 }
