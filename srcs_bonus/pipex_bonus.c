74c6c3b64efa70f03c585f09e3fa5fe8ba2ba9e1
Fabio Nacarelli <nacarellifabio@hotmail.com>
2022-12-30 20:10:28 -0300

    reading from stdin and appending to a tempfile

--- srcs_bonus/pipex_bonus.c
+++ srcs_bonus/pipex_bonus.c
@@ -6,24 +6,51 @@
 /*   By: fnacarel <fnacarel@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 14:13:45 by fnacarel          #+#    #+#             */
-/*   Updated: 2022/12/30 15:38:07 by fnacarel         ###   ########.fr       */
+/*   Updated: 2022/12/30 19:59:00 by fnacarel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/pipex_bonus.h"
+#include <unistd.h>
 
 static void	print_not_found_cmds(t_pipex pipex);
 static int	no_heredoc(int argc, char **argv, char **envp);
+static int	heredoc_pipex(char **argv);
 
 int	main(int argc, char **argv, char **envp)
 {
 	int	ret_code;
 
 	validate_input(argc, argv, envp);
-	if (ft_strncmp(argv[1], "here_doc", 8) == 0)
-	{}
+	if (ft_strncmp(argv[1], "here_doc", 9) == 0)
+		ret_code = heredoc_pipex(argv);
 	else
 		ret_code = no_heredoc(argc, argv, envp);
 	return (ret_code);
+}
+
+static int	heredoc_pipex(char **argv)
+{
+	int 	tempfile;
+	char	*gnl_ret;
+	char	*argv_with_nl;
+
+	tempfile = open(".tmpheredocz", O_CREAT | O_APPEND | O_WRONLY, 0644);
+	argv_with_nl = ft_strjoin(argv[2], "\n");
+	while (1)
+	{
+		write(1, "heredoc> ", 9);
+		gnl_ret = get_next_line(0, 0);
+		if (!gnl_ret || ft_strcmp(gnl_ret, argv_with_nl) == 0)
+		{
+			get_next_line(0, 1);
+			free(argv_with_nl);
+			free(gnl_ret);
+			break ;
+		}
+		write(tempfile, gnl_ret, ft_strlen(gnl_ret));
+		free(gnl_ret);
+	}
+	return (0);
 }
 
 static int	no_heredoc(int argc, char **argv, char **envp)
